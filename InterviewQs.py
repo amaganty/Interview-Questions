ab63002e475d6645b228c6fff72e0992f2c3a80d
Abhinav Maganty <abhinavmaganty@Abhinavs-MBP-2.hitronhub.home>
2021-07-16 15:08:16 -0700

    update1

--- InterviewQs.py
+++ InterviewQs.py
@@ -1,1 +1,18 @@
-#Interview Questions (Data Structures)
+# Interview Questions (Data Structures and Algorithms) 
+
+# Check if Number is a Palindrome
+
+num = int(input("Enter a number:"))
+temp  = num
+reverse = 0
+while (num > 0):
+    dig = num%10
+    reverse = reverse*10 + dig
+    num = num//10
+if (temp == reverse):
+    print ("Number is a palindrome!")
+else:
+    print ("Number is NOT a palindrome")
+
+# Check if String is a Palindrome
+
