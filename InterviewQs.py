121f38913b3bfab17bedef4cb4f16a06c5922985
Abhinav Maganty <abhinavmaganty@Abhinavs-MBP-2.hitronhub.home>
2021-07-17 18:23:01 -0700

    n

--- InterviewQs.py
+++ InterviewQs.py
@@ -40,6 +40,8 @@
 #         return a 
 # print(reverse(123))
 
+###################################################################################
+###################################################################################
 
 # ARRAY MANIPULATION AND SUM
 
@@ -88,3 +90,77 @@ if __name__ == "__main__" :
  
     print(getElement(a, n, S)) ;
 
+
+###################################################################################
+###################################################################################
+
+# Recursive binary search.
+  
+# Returns index of x in arr if present, else -1
+def binarySearch (arr, l, r, x):
+  
+    # Check base case
+    if r >= l:
+  
+        mid = l + (r - l) // 2
+  
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+          
+        # If element is smaller than mid, then it 
+        # can only be present in left subarray
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid-1, x)
+  
+        # Else the element can only be present 
+        # in right subarray
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+  
+    else:
+        # Element is not present in the array
+        return -1
+  
+# Driver Code
+arr = [ 2, 3, 10, 2, 40 ]
+x = 10
+  
+# Function call
+result = binarySearch(arr, 0, len(arr)-1, x)
+  
+if result != -1:
+    print ("Element is present at index % d" % result)
+else:
+    print ("Element is not present in array")
+
+###################################################################################
+###################################################################################
+
+# Compute Power
+# Given two integers x and n, write a function to compute xn.
+# We may assume that x and n are small and overflow doesn’t happen.
+# Python3 code for extended version
+# of power function that can work
+# for float x and negative y
+ 
+def power(x, y):
+ 
+    if(y == 0): 
+        return 1
+    temp = power(x, int(y / 2))
+     
+    if (y % 2 == 0):
+        return temp * temp
+    else:
+        if(y > 0): 
+            return x * temp * temp
+        else: 
+            return (temp * temp) / x
+     
+# Driver Code
+x, y = 2, 3
+print('%f' %(power(x, y)))
+ 
+###################################################################################
+###################################################################################
---
+++ SortAlgs.py
@@ -0,0 +1,8 @@
+# Sorting Algorithms
+
+#Insertion sort
+#Bubble sort
+#Merge sort
+#Selection sort
+#Quick sort
+ 
